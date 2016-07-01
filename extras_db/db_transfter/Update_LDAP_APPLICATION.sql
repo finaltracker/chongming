c98f0326eae809878888e81e887aeca365c7475e
finaltracker <amoi.jinxp@gmail.com>
2016-07-01 21:05:47 +0800

    添加修改LDAP APPLICATION的SQL

---
+++ extras_db/db_transfter/Update_LDAP_APPLICATION.sql
@@ -0,0 +1,8 @@
+update LADP_APPLICATION set steppage='/web/home/report.do' where stepname='报表结构';
+update LADP_APPLICATION set steppage='/web/home/country.do' where stepname='村镇管理';
+update LADP_APPLICATION set steppage='/web/home/level.do' where stepname='等级管理';
+update LADP_APPLICATION set steppage='/web/home/part.do' where stepname='考核部门';
+update LADP_APPLICATION set steppage='/web/home/person.do' where stepname='人员管理';
+update LADP_APPLICATION set steppage='/web/home/record.do' where stepname='考核管理';
+update LADP_APPLICATION set steppage='/web/home/statistics.do' where stepname='诚信统计';
+update LADP_APPLICATION set steppage='/web/home/log.do' where stepname='日志';

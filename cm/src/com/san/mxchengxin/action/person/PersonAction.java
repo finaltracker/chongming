1c0949b3b352e8a6b261bbbb41c138bcc0978ab8
finaltracker <amoi.jinxp@gmail.com>
2016-07-04 14:36:51 +0800

    Add Form

--- cm/src/com/san/mxchengxin/action/person/PersonAction.java
+++ cm/src/com/san/mxchengxin/action/person/PersonAction.java
@@ -1,7 +1,169 @@
 package com.san.mxchengxin.action.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import com.san.mxchengxin.form.person.PersonForm;
+import com.san.mxchengxin.model.country.CmCountry;
+import com.san.mxchengxin.model.country.CmCountryAd;
+import com.san.mxchengxin.model.country.CmCountryDAO;
+import com.san.mxchengxin.model.country.CmPerson;
+import com.san.mxchengxin.model.country.CmPersonDAO;
 
 public class PersonAction extends Action {
+	private CmCountryDAO cmCountryDAO;
+	private CmPersonDAO	cmPersonDAO;
+	List<CmCountry> countryList;
+	public CmCountryDAO getCmCountryDAO() {
+		return cmCountryDAO;
+	}
+	
+	public void setCmCountryDAO(CmCountryDAO cmCountryDAO) {
+		this.cmCountryDAO = cmCountryDAO;
+	}
+	
+	public CmPersonDAO getCmPersonDAO() {
+		return cmPersonDAO;
+	}
+	
+	public void setCmPersonDAO(CmPersonDAO cmPersonDAO) {
+		this.cmPersonDAO = cmPersonDAO;
+	}
+	
+	private String getCountrySelect(Short selectedId, Short parentId, int level) {
+		if(countryList.size() == 1)
+		{
+			CmCountry cc = (CmCountry)countryList.get(0);
+			String isSelected = "";
+			if(cc.getId().shortValue() == selectedId) {
+				isSelected = " selected";
+			}
+			
+			return String.format("<option value='%d'%s>%s</option>", cc.getId(),isSelected,"├─"+cc.getName());
+		}
+		
+		String select = "";
+		for(int i=0;i<countryList.size();i++) {
+			CmCountry cc = (CmCountry)countryList.get(i);
+			if(cc.getParentid() != parentId.shortValue()) continue;
+			
+			String empty="";
+			for(int x=1;x<level;x++)
+				empty +="&nbsp;&nbsp;";
+			
+			String isSelected = "";
+			if(cc.getId().shortValue() == selectedId) {
+				isSelected = " selected";
+			}
+			
+			select += String.format("<option value='%d'%s>%s</option>", cc.getId(),isSelected,empty+"├─"+cc.getName());
+			//System.out.println("temp select : "+select);
+			select += getCountrySelect(selectedId,cc.getId(),level+1);
+		}
+		
+		return select;
+	}
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		
+		if(request.getParameter("id") != null) {
+			Short countryId = Short.valueOf(request.getParameter("id"));
+			System.out.println("[person delete] id : "+countryId);
+			CmCountry cc = cmCountryDAO.findById(countryId);
+			boolean canDelete = true;
+			String tipMessage ="";
+			
+			List<CmCountry> beforeList = cmCountryDAO.findAll();
+			for(int i=0;i<beforeList.size();i++) {
+				CmCountry item = beforeList.get(i);
+				if(countryId == item.getParentid().shortValue()) {
+					System.out.println("has other country, so not delete ");
+					tipMessage = "该村镇下存在其它村不能直接删除";
+					canDelete = false;
+				}
+			}
+			//todo: add person restriction
+			List<CmPerson> beforeList2 = cmPersonDAO.findAll();
+			for(int i=0;i<beforeList.size();i++) {
+				CmPerson item = beforeList2.get(i);
+				if(countryId == item.getCountryId().shortValue()) {
+					System.out.println("has people, so not delete ");
+					tipMessage = "该村镇下存在相关人员不能直接删除";
+					canDelete = false;
+				}
+			}
+			
+			if(canDelete) {
+				System.out.println("we can delete ");
+				cmCountryDAO.delete(cmCountryDAO.findById(countryId));
+			} else {
+				request.setAttribute("tipMessage", tipMessage);
+			}
 
+		}
+		
+		String personTrueName = "";
+		String personSsid = "";
+		Short countryId = 0 ;
+		PersonForm personForm = (PersonForm)form;
+		personSsid = personForm.getPerson_ssid();
+		personTrueName = personForm.getPerson_truename();
+		countryId = personForm.getCountry_id();
+		
+		
+		countryList = cmCountryDAO.findAll();
+		
+		DetachedCriteria searDc =	DetachedCriteria.forClass( CmCountry.class);
+		
+		if( countryId == null ) 
+		{
+			countryId = 0;
+		} else if( countryId != 0 ) 
+		{
+			searDc.add(Restrictions.eq("parentid", countryId.shortValue() )); 
+		}
+		
+		if( personTrueName != null && ( !personTrueName.isEmpty() ))
+		{
+			searDc.add(Restrictions.like("name", personTrueName,MatchMode.ANYWHERE).ignoreCase()); 
+		}
+		
+		searDc.addOrder( Order.asc("displayOrder") ).addOrder( Order.asc("id") );
+		List<CmCountry> targetList = cmCountryDAO.getHibernateTemplate ().findByCriteria( searDc );
+		
+		List<CmCountryAd> cadList = new ArrayList<CmCountryAd>();
+		for(int i=0;i<targetList.size();i++) {
+			CmCountry target = (CmCountry)targetList.get(i);
+			CmCountryAd cca = new CmCountryAd(target);
+			if (target.getParentid().shortValue() == 0) {
+				cca.setParentName("崇明县");
+			}else {
+				cca.setParentName("崇明县>"+cmCountryDAO.findById(target.getParentid()).getName());
+			}
+			cadList.add(cca);
+		}
+		
+		
+		Short parentId = 0;
+		String countrySelect = getCountrySelect(countryId.shortValue(),parentId,1);
+		System.out.println("country select result: "+countrySelect);
+		request.setAttribute("countrySelect", countrySelect);
+		request.setAttribute("person_truename", personTrueName);
+		request.setAttribute("ssid", personSsid);
+		request.setAttribute("clist", cadList);
+		
+		return mapping.findForward( "personForword" );
+	}
 }
--- cm/src/com/san/mxchengxin/action/person/PersonAddAction.java
+++ cm/src/com/san/mxchengxin/action/person/PersonAddAction.java
@@ -1,7 +1,156 @@
 package com.san.mxchengxin.action.person;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.san.mxchengxin.form.country.CtyAddForm;
+import com.san.mxchengxin.model.country.CmCountry;
+import com.san.mxchengxin.model.country.CmCountryDAO;
+import com.san.mxchengxin.model.country.CmPersonDAO;
+import com.san.share.pmi.dto.LoginUserInfo;
+import com.san.share.pmi.service.LoginUserInfoDelegate;
 
 public class PersonAddAction extends Action {
+	private CmCountryDAO cmCountryDAO;
+	private CmPersonDAO cmPersonDAO;
+	List<CmCountry> countryList;
+	public CmCountryDAO getCmCountryDAO() {
+		return cmCountryDAO;
+	}
+	
+	public void setCmCountryDAO(CmCountryDAO cmCountryDAO) {
+		this.cmCountryDAO = cmCountryDAO;
+	}
+	
+	public CmPersonDAO getCmPersonDAO() {
+		return cmPersonDAO;
+	}
+	
+	public void setCmPersonDAO(CmPersonDAO cmPersonDAO) {
+		this.cmPersonDAO = cmPersonDAO;
+	}
+	private ActionForward add(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		
+		CtyAddForm addF = (CtyAddForm)form;
+		String name = addF.getCountry_name();
+		Short parentid = addF.getParent_id();
+		String contact = addF.getCountry_contact();
+		String phone = addF.getCountry_phone();
+		String displayOrder = addF.getCountry_display_order();
+		
+		if (name != null && parentid != null && contact != null && phone !=null && displayOrder !=null) {
+			CmCountry ct = new CmCountry();
+			ct.setName(name);
+			ct.setParentid(parentid);
+			ct.setContact(contact);
+			ct.setPhone(phone);
+			ct.setDisplayOrder(Short.valueOf(displayOrder));
+			
+			LoginUserInfo userInfo = LoginUserInfoDelegate.getLoginUserInfoFromRequest(request);
+			ct.setAuthor(userInfo.getCn());
+			
+			
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+			Date now = new Date();
+			System.out.println(df.format(now));// new Date()为获取当前系统时间
+			ct.setPubdate(now.getTime()/1000);
+			
+			cmCountryDAO.save(ct);
+		} else {
+			
+			List<CmCountry> afterList = cmCountryDAO.queryParentZero();
+			request.setAttribute("parentlist", afterList);
+		}
+			
+		return mapping.findForward("ctyaddForword");
+	}
+	
+	private ActionForward update(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		
+		if(request.getParameter("id")!=null &&!request.getParameter("id").isEmpty()) {
+			Short countryId = Short.valueOf(request.getParameter("id"));
+			System.out.println("[update] id : "+ countryId);
+			
+			CmCountry updateCc = cmCountryDAO.findById(countryId);
+			
+			request.setAttribute("country_name", updateCc.getName());
+			request.setAttribute("country_contact", updateCc.getContact());
+			request.setAttribute("country_phone", updateCc.getPhone());
+			request.setAttribute("country_display_order", updateCc.getDisplayOrder());
+			
+			List<CmCountry> afterList = cmCountryDAO.queryParentZero();
+			request.setAttribute("parentlist", afterList);
+			request.setAttribute("country_id", countryId);
+			request.setAttribute("ppid", updateCc.getParentid());
+			
+		}
+		
+		if(request.getParameter("xid")!=null &&!request.getParameter("xid").isEmpty()) {
+			CtyAddForm addF = (CtyAddForm)form;
+			String name = addF.getCountry_name();
+			Short parentid = addF.getParent_id();
+			String contact = addF.getCountry_contact();
+			String phone = addF.getCountry_phone();
+			String displayOrder = addF.getCountry_display_order();
+			
+			if (name != null && parentid != null && contact != null && phone !=null && displayOrder !=null) {
+				CmCountry ct = new CmCountry();
+				ct.setName(name);
+				ct.setParentid(parentid);
+				ct.setContact(contact);
+				ct.setPhone(phone);
+				ct.setDisplayOrder(Short.valueOf(displayOrder));
 
+				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+				Date now = new Date();
+				System.out.println(df.format(now));// new Date()为获取当前系统时间
+				ct.setPubdate(now.getTime()/1000);
+				Short xId = Short.valueOf(request.getParameter("xid"));
+				System.out.println("[update item] : "+xId);
+				ct.setId(xId);
+				LoginUserInfo userInfo = LoginUserInfoDelegate.getLoginUserInfoFromRequest(request);
+				ct.setAuthor(userInfo.getCn());
+				cmCountryDAO.update(ct);
+			}
+		}
+		return mapping.findForward("ctyaddForword");
+	}
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		
+		if(request.getParameter("method") != null && !request.getParameter("method").isEmpty()) {
+			Short actionMethod = Short.valueOf(request.getParameter("method"));
+			System.out.println("op action : "+actionMethod);
+			if(actionMethod == 1) {
+				System.out.println("add action");
+				request.setAttribute("pageInfo_action", "增加");
+				request.setAttribute("action_method", 1);
+				return add(mapping,form,request,response);
+			} else if (actionMethod == 3) {
+				System.out.println("modify action");
+				request.setAttribute("pageInfo_action", "更新");
+				request.setAttribute("action_method", 3);
+				return update(mapping,form,request,response);
+			} else {
+				System.out.println("others action");
+			}
+		}
+		
+		List<CmCountry> afterList = cmCountryDAO.queryParentZero();
+		request.setAttribute("parentlist", afterList);
+		
+		return mapping.findForward( "ctyaddForword" );
+	}
 }

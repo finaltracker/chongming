fd08d0a85efdf680d95409477240a399c26172f1
finaltracker <amoi.jinxp@gmail.com>
2016-07-18 10:47:07 +0800

    更新图表统计的url

--- extras_db/db_transfter/Update_LDAP_APPLICATION.sql
+++ extras_db/db_transfter/Update_LDAP_APPLICATION.sql
@@ -1,4 +1,4 @@
-update LADP_APPLICATION set steppage='/web/home/report.do' where stepname='报表结构';
+update LADP_APPLICATION set steppage='/web/home/report.do' where stepname='图表统计';
 update LADP_APPLICATION set steppage='/web/home/country.do' where stepname='村镇管理';
 update LADP_APPLICATION set steppage='/web/home/level.do' where stepname='等级管理';
 update LADP_APPLICATION set steppage='/web/home/part.do' where stepname='考核部门';
